d37abb05fcd6ce5cfc3e5ca8043bdba2f812016d
James Pettis <andrewhenry10@gmail.com>
2017-07-18 18:36:05 -0700

    guess who's following along with the instructions in the camp to the letter its me

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by faithpettis on 7/18/17.
+ */
+
+public class BasicOpMode extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
