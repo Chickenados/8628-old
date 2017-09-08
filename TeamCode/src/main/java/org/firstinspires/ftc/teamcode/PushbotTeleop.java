9e29cc1860f16c17b482896af8f126fbfaaf8846
James Pettis <andrewhenry10@gmail.com>
2017-09-08 16:48:52 -0700

    reversed left stick y in pushbot teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotTeleop.java
@@ -129,8 +129,10 @@ public class PushbotTeleop extends OpMode{
         }
 
         // Run wheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
-        left = -gamepad1.left_stick_y;
+        left = gamepad1.left_stick_y;
         right = -gamepad1.right_stick_y;
+        Range.clip(left, -1, 1);
+        Range.clip(right, -1, 1);
         robot.leftMotor.setPower(left);
         robot.rightMotor.setPower(right);
 
